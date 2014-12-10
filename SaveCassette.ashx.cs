f187fceb05590a521fc9b211d02ab02cafb9da87
mike_tinnes@hotmail.com <tinman0>
2014-12-10 14:35:51 -0600

    initial commit

---
+++ SaveCassette.ashx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace MC10
+{
+    /// <summary>
+    /// Summary description for SaveCassette
+    /// </summary>
+    public class SaveCassette : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var fname = context.Request["filename"];
+            var data = context.Request["cassette-data"];
+            var base64CharArray = data.ToArray();
+
+            byte[] binaryData;
+            binaryData = System.Convert.FromBase64CharArray(base64CharArray, 0, base64CharArray.Length);
+
+            context.Response.Clear();
+            context.Response.AppendHeader("Content-Disposition", String.Format("attachment; filename={0}.c10", fname.ToUpper()));
+            context.Response.ContentType = "application/octet-stream";
+            context.Response.BinaryWrite(binaryData);
+            context.Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
